82830cf6ca8709f22e1e3b2517d6be50a8f3dac4
jefedigital <36110645+jefedigital@users.noreply.github.com>
2020-12-12 12:56:12 -0500

    updates

---
+++ client_modules/vote_planetminecraft/recipe.py
@@ -0,0 +1,23 @@
+from api_classes.selenium import Selenium
+import os
+import re
+import time
+import yaml
+
+class Recipe():
+  def __init__(self, client_module, tmp_folder, browser):
+    
+    # get configs
+    with open(client_module + '/config.yml', 'r') as config:
+      cfg = yaml.full_load(config)
+    
+    # open session
+    api = Selenium(tmp_folder, browser)
+
+    # vote (no login needed)
+    api.get_url(cfg['url_vote'])
+
+    time.sleep(5)
+
+    api.set_input_value('//input[@name="mcname"]', cfg['vote_name'])
+    api.click('//input[@class="r3submit"]')
