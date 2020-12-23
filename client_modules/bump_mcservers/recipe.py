bd072a109a02806583c9933f2875e52c7c3bf996
jefedigital <36110645+jefedigital@users.noreply.github.com>
2020-12-23 18:43:26 -0500

    added bump_mcservers

---
+++ client_modules/bump_mcservers/recipe.py
@@ -0,0 +1,30 @@
+from api_classes.selenium import Selenium
+import os
+import re
+import time
+import yaml
+
+class Recipe():
+  def __init__(self, client_module, tmp_folder, browser):
+    
+    # get configs
+    with open(client_module + '/config.yml', 'r') as config:
+      cfg = yaml.full_load(config)
+
+    # open browser
+    api = Selenium(tmp_folder, browser)
+    
+    # login
+    api.get_url(cfg['url_login'])
+    time.sleep(3)
+
+    api.set_input_value('//input[contains(@name,"username")]', cfg['username'])
+    api.set_input_value('//input[contains(@name,"password")]', cfg['password'])
+    api.click('//button[@type="submit"][contains(.,"Login")]')
+    print('logged in')
+
+    # bump (login needed)
+    api.get_url(cfg['url_bump'])
+    time.sleep(3)
+
+    print(cfg['site'] + ' bumped')
