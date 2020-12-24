f91f78a4ab63593549d0662a3c9b0b4260d4a787
jefedigital <36110645+jefedigital@users.noreply.github.com>
2020-12-23 19:45:17 -0500

    more

--- client_modules/minecraftserver_bump/recipe.py
+++ client_modules/minecraftserver_bump/recipe.py
@@ -28,6 +28,5 @@ class Recipe():
     api.get_url(cfg['url_bump'])
     time.sleep(3)
 
-    api.click('//input[@name="bump_username"]')
+    api.click('//input[@value="delphicraft"]')
     print(cfg['site'] + ' bumped')
-    
