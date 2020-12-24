66b9301b0603754b1365a1a5c373a10477dce167
jefedigital <36110645+jefedigital@users.noreply.github.com>
2020-12-24 12:19:07 -0500

    fixed minecraftserver_bump

--- client_modules/minecraftserver_bump/recipe.py
+++ client_modules/minecraftserver_bump/recipe.py
@@ -28,5 +28,5 @@ class Recipe():
     api.get_url(cfg['url_bump'])
     time.sleep(3)
 
-    api.click('//input[contains(@value,"Bump your Server")]')
+    api.click('//input[contains(@value,"Bump Server to Top")]')
     print(cfg['site'] + ' bumped')
